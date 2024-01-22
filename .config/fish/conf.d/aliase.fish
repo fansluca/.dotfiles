bcd2456f2912eb959aa20f01060ec3f78958ad17
fansluca <witherslove@gmail.com>
2024-01-22 14:03:41 +0800

    fish: add fish_greeting.fish

--- .config/fish/conf.d/aliase.fish
+++ .config/fish/conf.d/aliase.fish
@@ -25,16 +25,13 @@ end
 
 function ya
     set -l tmp (mktemp -t "yazi-cwd.XXXXX")
-
     yazi $argv --cwd-file="$tmp"
-
     if test -f "$tmp"
         set -l cwd (cat -- "$tmp")
         if test -n "$cwd"; and test "$cwd" != "$PWD"
             cd -- "$cwd"
         end
     end
-
     rm -f -- "$tmp"
 end
 
