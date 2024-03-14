6006cce0f08461d0e823185733d841f08c6eceaf
fansluca <witherslove@gmail.com>
2024-03-14 19:47:51 +0800

    hypr: use systemd environment

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,4 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    export PATH="$PATH:$HOME/.local/bin"
-
     # ~/.config/fish/config.fish
     starship init fish | source
 
