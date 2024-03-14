455af880baea2d5124d563e11199b7ad28083d2f
fansluca <witherslove@gmail.com>
2024-03-14 21:28:48 +0800

    hypr: remove ~/.config/environment.d

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,11 @@
 if status is-interactive
+    # Commands to run in interactive sessions can go here
+    export PATH="$PATH:$HOME/.local/bin"
+
     # ~/.config/fish/config.fish
     starship init fish | source
 
     set -x _ZO_ECHO 1
     zoxide init --cmd cd fish | source
+
 end
--- .config/hypr/scripts/XDPH.sh
+++ .config/hypr/scripts/XDPH.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 sleep 1
+
+# kill all possible running xdg-desktop-portals
 killall -e xdg-desktop-portal-hyprland
 killall -e xdg-desktop-portal-wlr
+killall xdg-desktop-portal-gnome
+killall xdg-desktop-portal-kde
+killall xdg-desktop-portal-gtk
 killall xdg-desktop-portal
+
+# start xdg-desktop-portal-hyprland
 /usr/lib/xdg-desktop-portal-hyprland &
 sleep 2
+
+# start xdg-desktop-portal
 /usr/lib/xdg-desktop-portal &
